42b6fbe28ba2a7a7b3deb7274bbbea72e009ab14
Sagar <sawantsagar224@gmail.com>
2026-06-12 12:16:39 +0530

    updated s3 sync commands

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -17,7 +17,7 @@ function Home() {
           <h1 className="home__heading">Hello</h1>
 
           <h2 className="home__subheading">
-            A Bit About Me
+            A Bit About Me LOL
           </h2>
 
           <p className="home__text">
