1bf2d299bfb8ffb8b16340eafe0b116497dc5678
Sagar <sawantsagar224@gmail.com>
2026-06-15 18:46:08 +0530

    Changed the resume file name

--- src/components/pages/Resume/Resume.tsx
+++ src/components/pages/Resume/Resume.tsx
@@ -72,7 +72,7 @@ const Resume = () => {
           >
             {focusMode ? "Exit Focus Mode" : "Focus Mode"}
           </button>
-          <a className="resume__download" href="/Sagar_Sawant_Software_Engineer_Resume.pdf" download>
+          <a className="resume__download" href="/SAGAR_SAWANT_Software_Engineer_Resume.pdf" download>
             <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="7 10 12 15 17 10" /><line x1="12" y1="15" x2="12" y2="3" /></svg>
             Download PDF
           </a>
